5ac88c802b200074d9362fb4c7454c04e57f1513
tsuyukusa-rk <rk.1227.89@gmail.com>
2015-12-14 14:09:46 +0900

    スタイルの調整とタブ切り替え処理を若干記述

--- assets/js/views/mainAreaTopView.js
+++ assets/js/views/mainAreaTopView.js
@@ -4,6 +4,7 @@ var mainAreaTopTmpl = require('../templates/mainAreaTopView');
 module.exports = Marionette.ItemView.extend({
 
     // 監視対象DOM
+    //イベントに紐付けるセレクターはelと相対的に指定出来る
     el: '#mainAreaTop',
 
     // 読み込むテンプレート
@@ -11,7 +12,12 @@ module.exports = Marionette.ItemView.extend({
 
     // イベント紐付け
     events: {
+        'click li': 'changeTab'
+    },
 
+    // タブ切り替え時の処理
+    changeTab: function() {
+        console.log('タブをクリック')
     }
 
 });
