9a6f4f9f2fc2bb94d2d97d10a1fc13dfd51a4740
tsuyukusa-rk <rl.1227.89@gmail.com>
2015-12-30 11:21:34 +0900

    再生機周りを固めた。overflow: hidden;はclassでまとめた方がいいかも

--- assets/js/views/mainAreaBottomView.js
+++ assets/js/views/mainAreaBottomView.js
@@ -33,8 +33,11 @@ module.exports = Marionette.ItemView.extend({
             this.$audiArea = $('#audioArea' + i),
             this.$play = $('#play' + i),
             this.$pause = $('#pause' + i),
+            this.$stop = $('#stop' + i),
             this.$time = $('#time' + i),
-            this.$currentTime = $('#currentTime' + i);
+            this.$currentTime = $('#currentTime' + i),
+            this.$prevCtrl = $('#prevCtrl' + i),
+            this.$nextCtrl = $('#nextCtrl' + i);
 
             // メタデータの読み込みが完了して、メディアリソースの長さと大きさが決まって
             // テキストトラックの準備が出来た時
@@ -53,6 +56,9 @@ module.exports = Marionette.ItemView.extend({
             var duration = Math.round(that.audio.duration);
             that.$time.html(Math.round(duration / 60) + ':' + duration % 60);
 
+            // 初期値は絶対に、0:00
+            that.$currentTime.html('0:00');
+
             // 再生
             that.$play.on('click', function() {
                 that.audio.play();
@@ -60,18 +66,35 @@ module.exports = Marionette.ItemView.extend({
 
             // 一時停止
             that.$pause.on('click', function() {
+                // 再生を止める
+                that.audio.pause();
+            });
+
+            // 停止
+            that.$stop.on('click', function() {
                 // 再生時間を0に戻す
                 that.audio.currentTime = 0;
                 that.audio.pause();
+            });
+
+            // 巻き戻し
+            that.$prevCtrl.on('click', function() {
+                // 再生時間を10戻す
+                that.audio.currentTime -= 10;
+            });
+
+            // 早送り
+            that.$nextCtrl.on('click', function() {
+                // 再生時間を10送る
+                that.audio.currentTime += 10;
             });
 
             // 再生時間に変更があった場合の処理
             that.$audio.on('timeupdate', function() {
 
                 // 現在の再生時間を取得
-                var
-                    currentTime = Math.round(that.audio.currentTime),
-                    t;
+                var currentTime = Math.round(that.audio.currentTime);
+                var t;
 
                 // 1分未満の場合
                 if(currentTime < 60) {
