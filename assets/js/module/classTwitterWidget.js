db5d249f366d898cb75970c26ca7df0545046ef8
tsuyukusa-rk <rl.1227.89@gmail.com>
2016-01-03 15:23:24 +0900

    レイアウト修正

--- assets/js/module/classTwitterWidget.js
+++ assets/js/module/classTwitterWidget.js
@@ -15,8 +15,15 @@ module.exports = class changeWidget {
 
         if ($twFrame.length > 0) {
             // スタイルの調整
-            $twFrame.contents().find('body').css({
-                "font-family": "'Noto Sans Japanese', 'メイリオ', sans-serif"
+            $twFrame.contents().find('html, body, .sandboxroot, h1, p, ul, li, iframe, button').css({
+                "font-family": "'Noto Sans Japanese', 'メイリオ', sans-serif",
+                color: '#777777'
+            });
+            $twFrame.contents().find('.header').css({
+                "margin": "0 0 5px"
+            });
+            $twFrame.contents().find('.e-entry-title').css({
+                'font-size': '12px'
             });
         } else {
             // 見つかるまで繰り返す
